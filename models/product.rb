624357a0dec1de59f264a497ad70d0976356fc47
Maria <mariajescobar10@gmail.com>
2018-01-29 21:31:22 -0600

    more stuff

--- models/product.rb
+++ models/product.rb
@@ -9,7 +9,15 @@ class Product
     @tax = input_options["tax"]
     @total = input_options["total"]
     @price = input_options["price"]
+    
     @formatted_price = input_options["formatted_price"]
+    @formatted_tax = input_options["formatted_tax"]
+    @formatted_total = input_options["formatted_total"]
+
+    @supplier_name = input_options["supplier"]["name"]
+    @supplier_id = input_options["supplier"]["id"]
+
+    @images_urls = input_options["images"].map { |image_hash| image_hash["url"] }
   end
 
   def self.convert_hashs(product_hashs)
